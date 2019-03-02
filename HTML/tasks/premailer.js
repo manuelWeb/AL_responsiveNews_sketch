462cbc6f2bd80846700ae65dbcbe16c619a65651
macfly <manuel@webdesignord.fr>
2019-03-02 16:24:27 +0100

    fix sass font-stack escape &quote; in compound font names -> premailer.js

--- HTML/tasks/premailer.js
+++ HTML/tasks/premailer.js
@@ -1,27 +1,30 @@
-var gulp      = require("gulp");
+var gulp = require('gulp')
 var premailer = require('gulp-premailer'),
-    replace   = require('gulp-replace');
+  replace = require('gulp-replace')
 
-module.exports = function () {
+module.exports = function() {
   // promise = start prettify
-  gulp.task('premailer', function () {
+  gulp.task('premailer', function() {
     return Promise.all([
-      new Promise(function (resolve, reject) {
-        gulp.src('render/**/*.html')
-        .pipe(premailer({
-            // escape_url_attributes: false,
-            // replace_html_entities: false
-        }))
-        .pipe(replace(/&amp;/g, '&'))
-        .pipe(replace(/~/g, '&nbsp;'))
-        .pipe(gulp.dest('render'))
-        // .pipe(bs.stream())
-        .on('end', resolve)
-      })
-    ]).then(function () {
+      new Promise(function(resolve, reject) {
+        gulp
+          .src('render/**/*.html')
+          .pipe(
+            premailer({
+              // escape_url_attributes: false,
+              // replace_html_entities: false
+            })
+          )
+          .pipe(replace(/&amp;/g, '&'))
+          .pipe(replace('&quot;', "'"))
+          // .pipe(replace(/~/g, '&nbsp;'))
+          .pipe(gulp.dest('render'))
+          // .pipe(bs.stream())
+          .on('end', resolve)
+      }),
+    ]).then(function() {
       console.log('premailer terminé run prettify + bs')
-      gulp.start('prettify');
+      gulp.start('prettify')
     })
-  });
-
+  })
 }
